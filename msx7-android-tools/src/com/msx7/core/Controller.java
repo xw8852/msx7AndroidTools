70c6713a8aeced381e81977954f3fd52f274609d
xiaowei <xw8852@126.com>
2013-03-26 23:25:10 +0800

    添加图片缓存
    添加图片缓存

--- msx7-android-tools/src/com/msx7/core/Controller.java
+++ msx7-android-tools/src/com/msx7/core/Controller.java
@@ -2,22 +2,31 @@ package com.msx7.core;
 
 import android.app.Application;
 import android.os.Handler;
+import android.util.DisplayMetrics;
 
 public class Controller extends Application {
-	private static Controller instance;
-	private Handler mHandler;
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		instance = this;
-	}
+    private static Controller instance;
+    private Handler mHandler;
+    public static final boolean DEBUG = true;
+    public static int WIDTH=0;
+    public static int HEIGHT=0;
+    DisplayMetrics metrics;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        metrics =getResources().getDisplayMetrics();
+        WIDTH=metrics.widthPixels;
+        HEIGHT= metrics.heightPixels;
+    }
 
-	public static final Controller getApplication() {
-		return instance;
-	}
+    public static final Controller getApplication() {
+        return instance;
+    }
 
-	public Handler getHandler(){
-		if(mHandler==null) mHandler=new Handler();
-		return mHandler;
-	}
+    public Handler getHandler() {
+        if (mHandler == null)
+            mHandler = new Handler();
+        return mHandler;
+    }
 }
